1781eaded4285e3798526c4e35d7daca9cd9e1af
DevExpressExampleBot <fakeEmail>
2020-07-13 09:47:53 +0300

    autogenerate VB

---
+++ VB/SecuritySetState.Module/BusinessObjects/Contact.vb
@@ -0,0 +1,90 @@
+﻿Imports System
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.Xpo
+Imports DevExpress.ExpressApp
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.DC
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.Base
+Imports System.Collections.Generic
+Imports DevExpress.ExpressApp.Model
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.Validation
+Imports DevExpress.ExpressApp.Editors
+Imports System.Diagnostics
+Imports DevExpress.Persistent.Base.General
+Imports DevExpress.ExpressApp.SystemModule
+Imports DevExpress.ExpressApp.ConditionalAppearance
+
+Namespace SecuritySetState.Module.BusinessObjects
+	<DefaultClassOptions>
+	<DebuggerDisplay("FirstName: {FirstName}")>
+	<Appearance("RedPriceObject", AppearanceItemType := "ViewItem", TargetItems := "TestBool,LastName", Enabled := False)>
+	Public Class Contact
+		Inherits BaseObject
+
+		Public Sub New(ByVal session As Session)
+			MyBase.New(session)
+		End Sub
+		Public Overrides Sub AfterConstruction()
+			MyBase.AfterConstruction()
+		End Sub
+'INSTANT VB NOTE: The field firstName was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private firstName_Conflict As String
+		Public Property FirstName() As String
+			Get
+				Return firstName_Conflict
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue(NameOf(FirstName), firstName_Conflict, value)
+			End Set
+		End Property
+'INSTANT VB NOTE: The field lastName was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private lastName_Conflict As String
+		Public Property LastName() As String
+			Get
+				Return lastName_Conflict
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue(NameOf(LastName), lastName_Conflict, value)
+			End Set
+		End Property
+'INSTANT VB NOTE: The field age was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private age_Conflict As Integer
+		Public Property Age() As Integer
+			Get
+				Return age_Conflict
+			End Get
+			Set(ByVal value As Integer)
+				SetPropertyValue(NameOf(Age), age_Conflict, value)
+			End Set
+		End Property
+'INSTANT VB NOTE: The field testBool was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private testBool_Conflict As Boolean
+		Public Property TestBool() As Boolean
+			Get
+				Return testBool_Conflict
+			End Get
+			Set(ByVal value As Boolean)
+				SetPropertyValue(NameOf(TestBool), testBool_Conflict, value)
+			End Set
+		End Property
+		' DateTime _birthDate;
+		' public DateTime BirthDate {
+		' get {
+		' return _birthDate;
+		' }
+		' set {
+		' SetPropertyValue(nameof(BirthDate), ref _birthDate, value);
+		' }
+		' }	
+		'office#3		
+		<Association("Contact-Tasks")>
+		Public ReadOnly Property Tasks() As XPCollection(Of MyTask)
+			Get
+				Return GetCollection(Of MyTask)(NameOf(Tasks))
+			End Get
+		End Property
+	End Class
+End Namespace
---
+++ VB/SecuritySetState.Module/Controllers/SetStateController.vb
@@ -0,0 +1,49 @@
+﻿Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.SystemModule
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl.PermissionPolicy
+
+Namespace SecuritySetState.Module.Controllers
+	Public Class SetStateController
+		Inherits ViewController
+
+		Public Sub New()
+			TargetViewId = "PermissionPolicyRoleBase_TypePermissions_ListView;PermissionPolicyRoleBase_NavigationPermissions_ListView"
+		End Sub
+		Private controller As NewObjectViewController
+		Protected Overrides Sub OnActivated()
+			MyBase.OnActivated()
+			controller = Frame.GetController(Of NewObjectViewController)()
+			If controller IsNot Nothing Then
+				AddHandler controller.ObjectCreated, AddressOf controller_ObjectCreated
+			End If
+		End Sub
+		Private Sub controller_ObjectCreated(ByVal sender As Object, ByVal e As ObjectCreatedEventArgs)
+			Dim typePermission As PermissionPolicyTypePermissionObject = TryCast(e.CreatedObject, PermissionPolicyTypePermissionObject)
+			If typePermission IsNot Nothing Then
+				Dim defaultState As SecurityPermissionState = SecurityPermissionState.Allow
+				If typePermission.Role.PermissionPolicy = SecurityPermissionPolicy.AllowAllByDefault Then
+					defaultState = SecurityPermissionState.Deny
+				End If
+				typePermission.ReadState = defaultState
+				typePermission.WriteState = defaultState
+				typePermission.CreateState = defaultState
+				typePermission.DeleteState = defaultState
+			End If
+			Dim navigationPermission As PermissionPolicyNavigationPermissionObject = TryCast(e.CreatedObject, PermissionPolicyNavigationPermissionObject)
+			If navigationPermission IsNot Nothing Then
+				Dim defaultState As SecurityPermissionState = SecurityPermissionState.Allow
+				If navigationPermission.Role.PermissionPolicy = SecurityPermissionPolicy.AllowAllByDefault Then
+					defaultState = SecurityPermissionState.Deny
+				End If
+				navigationPermission.NavigateState = defaultState
+			End If
+		End Sub
+		Protected Overrides Sub OnDeactivated()
+			If controller IsNot Nothing Then
+				RemoveHandler controller.ObjectCreated, AddressOf controller_ObjectCreated
+			End If
+			MyBase.OnDeactivated()
+		End Sub
+	End Class
+End Namespace
