db7992979719e1f5e75a252ef03497fb02b72a7d
DevExpressExampleBot <fakeEmail>
2021-12-14 12:35:45 +0300

    autogenerate VB

--- VB/SecuritySetState.Module.Win/WinModule.Designer.vb
+++ VB/SecuritySetState.Module.Win/WinModule.Designer.vb
@@ -1,36 +1,37 @@
-﻿Namespace SecuritySetState.Module.Win
-	Partial Public Class SecuritySetStateWindowsFormsModule
-		''' <summary> 
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace SecuritySetState.[Module].Win
 
-		''' <summary> 
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class SecuritySetStateWindowsFormsModule
 
-		#Region "Component Designer generated code"
+        ''' <summary> 
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary> 
-		''' Required method for Designer support - do not modify 
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			' 
-			' SecuritySetStateWindowsFormsModule
-			' 
-			Me.RequiredModuleTypes.Add(GetType(SecuritySetState.Module.SecuritySetStateModule))
-			Me.RequiredModuleTypes.Add(GetType(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule))
-			Me.RequiredModuleTypes.Add(GetType(DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule))
-		End Sub
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		#End Region
-	End Class
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Component Designer generated code"
+        ''' <summary> 
+        ''' Required method for Designer support - do not modify 
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            ' 
+            ' SecuritySetStateWindowsFormsModule
+            ' 
+            Me.RequiredModuleTypes.Add(GetType(SecuritySetState.[Module].SecuritySetStateModule))
+            Me.RequiredModuleTypes.Add(GetType(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule))
+            Me.RequiredModuleTypes.Add(GetType(DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule))
+        End Sub
+#End Region
+    End Class
 End Namespace
